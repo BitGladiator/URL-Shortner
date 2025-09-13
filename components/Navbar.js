1caedba49bed10cf3f8996c696279678c8c92d4d
Karan Sharma <karansh1232@gmail.com>
2025-09-13 17:54:45 +0530

    Initial Commit

---
+++ components/Navbar.js
@@ -0,0 +1,36 @@
+import Link from "next/link";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="h-16 bg-purple-700 flex items-center justify-between px-3 text-white ">
+      <div className="logo font-bold text-2xl">URL-Shortener</div>
+      <ul className="flex justify-center items-center gap-4">
+        <Link href="/">
+          <li>Home</li>
+        </Link>
+        <Link href="/about">
+          <li>About</li>
+        </Link>
+        <Link href="/generate">
+          <li>Shorten</li>
+        </Link>
+        <Link href="/contact">
+          <li>Contact Us</li>
+        </Link>
+        <li className="flex gap-3 ">
+          <Link href="/generate">
+            <button className="bg-purple-500 shadow-lg p-3 rounded-lg py-1 font-bold">Try Now</button>
+          </Link>
+          <Link href="/github">
+            <li>
+              <button className="bg-purple-500 shadow-lg p-3 rounded-lg py-1 font-bold">Github</button>
+            </li>
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
